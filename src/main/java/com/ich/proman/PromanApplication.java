dea018c8d9f98d963ff0f1126c2187db8caddfb3
HJ <huojun17603@163.com>
2018-03-15 22:09:30 +0800

    清除多余的引入

--- src/main/java/com/ich/proman/PromanApplication.java
+++ src/main/java/com/ich/proman/PromanApplication.java
@@ -1,14 +1,9 @@
 package com.ich.proman;
 
-import com.ich.config.processor.IConfigProcessor;
-import com.ich.international.processor.ILocaleInitProcessor;
-import com.ich.module.processor.ModularInitProcessor;
-import com.ich.module.processor.ResourcesInitProcessor;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
